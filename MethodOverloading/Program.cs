19b0ab2f809b8c882829193f0f8f00cede11c077
Jessica Gonzales <jgonzales0688@gmail.com>
2022-12-02 10:52:26 -0600

    Methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(2, 3));
+            Console.WriteLine(Add(2.0, 3.5));
+            Console.WriteLine(Add(2, 3, true));
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool withDollars)
+        {
+            if (withDollars && (x + y) > 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if (withDollars && (x + y) == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else
+            {
+                return $"{x + y}";
+            }
         }
     }
 }
+
+
